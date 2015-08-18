332a5982622a7b416fdc5b86db6e5b3cabe295f8
fai1618 <fai1.618033@gmail.com>
2015-08-18 19:19:23 +0900

    ラズベリーパイ用に

--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 var SerialPort = require('serialport').SerialPort;
 
-var irMagician = new SerialPort("/dev/tty.usbmodem0121", { baudrate: 9600 });
-//var irMagician = new SerialPort("/dev/ttyACM0", { baudrate: 9600 });
+//var irMagician = new SerialPort("/dev/tty.usbmodem0121", { baudrate: 9600 });
+var irMagician = new SerialPort("/dev/ttyACM0", { baudrate: 9600 });
 irMagician.on("open", function () {
     console.log('open');
     irMagician.on('data', function(data) {
