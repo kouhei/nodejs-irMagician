b3202121c1288f1a1e500166a531ccb1ab175f03
fai1618 <fai1.618033@gmail.com>
2015-10-21 03:03:34 +0900

    doorSeonsor.jsの主要部分を切り出してsrcに追加(モジュール化)
    raspiBで動作確認

--- util/doorSensor/doorSensor.js
+++ util/doorSensor/doorSensor.js
@@ -10,11 +10,9 @@ process.on('exit', function () {
   console.log('exit.');
 });
 
-var fs = require('fs'),
-    color = require('../../src/color'),
+var color = require('../../src/color'),
     IRMagician = require('../../irMagician'),
     irMagician = {},
-    port = '',
 
     SerialPort = require('serialport').SerialPort,
     arduino,
@@ -24,14 +22,30 @@ var fs = require('fs'),
 
     Mailer = require('../../src/mailer'),
     mailer,
-    mailerPass,
 
     GetDate = require('../../src/getDate.js'),
-    getDate = new GetDate();
+    getDate = new GetDate(),
 
+    doorSensor = require('../../src/doorSensor.js'),
+    processings = [];
 
-arduino = new SerialPort('/dev/ttyACM0');
-irMagician = new IRMagician('/dev/ttyACM1');//MEMO:arduinoと同時に繋いだ時
+arduino = new SerialPort('/dev/ttyACM1');
+irMagician = new IRMagician('/dev/ttyACM0');//MEMO:arduinoと同時に繋いだ時
+
+processings[0] = function(data,lightJudge){
+  switch(data){
+    case '0\n' :
+    case '0' : console.log(color.info('door is closed')); break;
+    case '1\n' :
+    case '1' :
+      console.log(color.info('[' + getDate.getTime()+'] door is opened'));
+      if (mailer) { mailer.send('Door is opened.'); }
+      lightJudge();
+      break;
+    case '' : break;
+    default:console.log(color.error('error'));
+  }
+};
 
 if(process.argv[2] && process.argv[3] && process.argv[4]){
   mailer = new Mailer(process.argv[2], process.argv[3], process.argv[4]);
@@ -40,42 +54,6 @@ if(process.argv[2] && process.argv[3] && process.argv[4]){
   console.log(color.error('mailer is not defined!'));
 }
 
-
-lightJudge = function(){//FIXME:引数にcountとしてcloseCountをインクリメントすると挙動がおかしい
-  var dataName = '';
-  lightJudge.closeCount++;
-  //console.log('count' + lightJudge.closeCount);
-  if(lightJudge.closeCount % 2 === 1){
-    dataName = '../../json/lightOn.json';
-  }else{
-    dataName = '../../json/lightOff.json';
-  }
-  irMagician.Lplay(dataName, function(){console.log('Lplay end callback');});
-};
-lightJudge.closeCount = 0;
-
-arduino.on('open', function(){
-    console.log(color.info('arduino is opened'));
-
-    arduino.on('data', function(data){
-        console.log('<data>' + data + '</data>');
-        data += '';//Object => String
-        switch(data){
-          case '0\n' :
-          case '0' : console.log(color.info('door is closed')); break;
-          case '1\n' :
-          case '1' :
-            console.log(color.info('[' + getDate.getTime()+'] door is opened'));
-            if (mailer) { mailer.send('Door is opened.'); }
-            lightJudge();
-            break;
-          default:console.log(color.error('error'));
-        }
-    });
-
-    arduino.on('close', function(){
-        console.log(color.warning('arduino is closed'));
-    });
-});
+doorSensor(irMagician, arduino, processings);
 //FIXME:ドアの開け閉めだけで判定するとめんどい
 //TODO:メール送信しなくていい時の判定追加
